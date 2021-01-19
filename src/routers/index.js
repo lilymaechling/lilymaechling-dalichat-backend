a5474eae1b83a8b1f8d86dfb8d3cf1e1628b8d53
Adam McQuilkin <46639306+ajmcquilkin@users.noreply.github.com>
2021-01-19 18:45:52 -0500

    Updated starter code

--- src/routers/index.js
+++
@@ -1,8 +0,0 @@
-import authRouter from './authRouter';
-import userRouter from './userRouter';
-import postRouter from './postRouter';
-import searchRouter from './searchRouter';
-
-export {
-  authRouter, userRouter, postRouter, searchRouter,
-};
--- src/utils/constants.js
+++
@@ -1,9 +0,0 @@
-/**
- * Creates successful deletion message for HTTP DELETE handlers
- * @param {string} id id to include in message
- */
-export function getSuccessfulDeletionMessage(id) {
-  return `User with id: ${id} was successfully deleted`;
-}
-
-export const DEFAULT_PORT = 9090;
